9f37acde4b3f18fcb12d1272e7fce44850bbadf8
Mark <mark.nelubin@gmail.com>
2021-06-10 19:33:29 +0300

    Adedd tabs functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Tooltip from './mark-ui/tooltip';
 import Dropdown from './mark-ui/dropdown';
+import Tabs from './mark-ui/tabs';
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -12,4 +13,8 @@ const dropdowns = document.querySelectorAll('.dropdown');
 dropdowns.forEach(dropdown => {
     const instance = new Dropdown(dropdown);
     instance.init();
-})
+});
+
+// create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
